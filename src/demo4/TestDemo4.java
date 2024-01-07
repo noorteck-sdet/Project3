8d249ec7c823c9ca44e5cf1330fc86e0e9230296
Ross <Ross@192.168.1.237>
2024-01-06 19:36:33 -0600

    Calculator Fun Time Initial Commit with Add Method

--- src/demo4/TestDemo4.java
+++ src/demo4/TestDemo4.java
@@ -3,5 +3,17 @@ package demo4;
 public class TestDemo4 {
 	public static void main(String[] args) {
 		System.out.println("Friday");
+		System.out.println("CALCULATOR FUN TIME"); 
+		
+		// add method by Alex
+		System.out.print("ADD method: ");
+		add(6, 7);
+		
 	}
+	
+	public static void add(int a, int b) {
+		System.out.println(a+" + " + b + " = "+ (a+b));
+	}
+	
+	
 }
