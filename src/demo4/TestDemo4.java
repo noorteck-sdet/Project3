b7c3a193f8df2d4c4f77905b68c8a6c594f826af
zalkh <zalkh@10.0.0.55>
2024-01-06 19:51:48 -0600

    Implement divide Method ()

--- src/demo4/TestDemo4.java
+++ src/demo4/TestDemo4.java
@@ -1,6 +1,10 @@
 package demo4;
 
 public class TestDemo4 {
+
+	double a;
+	double b;
+
 	public static void main(String[] args) {
 		System.out.println("Friday");
 		System.out.println("CALCULATOR FUN TIME"); 
@@ -8,11 +12,28 @@ public class TestDemo4 {
 		// add method by Alex
 		System.out.print("ADD method: ");
 		add(6, 7);
-		
+
+		// Divide method by zain
+		System.out.println();
+		TestDemo4 obj = new TestDemo4();
+		System.out.println("Divide Methods /:/");
+		double r1 = obj.divide(88, 20);
+		System.out.println("Result: "+ obj.a + " / "+ obj.b+ " = " + r1);
+
 	}
 	
 	public static void add(int a, int b) {
-		System.out.println(a+" + " + b + " = "+ (a+b));
+		System.out.println(a + " + " + b + " = " + (a + b));
+	}
+
+	public double divide(double a, double b) {
+
+		this.a = a;
+		this.b = b;
+
+		double result = a / b;
+
+		return result;
 	}
 	
 	
